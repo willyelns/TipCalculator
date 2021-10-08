d883ca6c6ca3ec69eaa0890daa453d453e3d4669
Will Xavier <willyelnsg.x@gmail.com>
2021-10-07 21:07:29 -0300

    Initial Commit

---
+++ TipCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TipCalculator
+//
+//  Created by Will Germano on 07/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
